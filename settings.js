7a957f3315c2fbe39a56089a1f185a76097f2e09
dasu992 <113847347+dasu992@users.noreply.github.com>
2022-09-18 21:17:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,8 +12,8 @@ global.APIKeys = {
 }
 
 
-global.OWNER_REACT = '🧑🏻‍💻'
-global.REACT_NUMBER = '94714188730'
+global.OWNER_REACT = '🍼'
+global.REACT_NUMBER = '94775978468'
 
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
@@ -34,23 +34,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['94714188730'] //ur owner number
-global.ownername = "NIPUNA " //ur owner name
-global.ytname = "YT: Nipuna-apps" //ur yt chanel name
-global.socialm = "GitHub: Nipuna-apps" //ur github or insta name
+global.vcardowner = ['94775978468'] //ur owner number
+global.ownername = "𝙼𝚁.𝚂𝙸𝙻𝙴𝙽" //ur owner name
+global.ytname = "YT: 𝚂𝙸𝙻𝙴𝙽 𝚃𝙴𝙲𝙷𝚂𝙷𝙾𝚆" //ur yt chanel name
+global.socialm = "GitHub: 𝚂𝙸𝙻𝙴𝙽𝙼𝙾𝙳𝚉" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['714188730']
-global.ownertag = '714188730' //ur tag number
-global.botname = '⚜𝓑𝓵𝓾𝓮 𝓛𝓲𝓸𝓷𝓮⚜' //ur bot name
-global.linkz = "https://github.com/Nipuna-apps/Blue-Lione-Bot.git" //your theme url which will be displayed on whatsapp
-global.websitex = "https://github.com/Nipuna-apps/Blue-Lione-Bot.git" //ur website to be displayed
-global.botscript = 'https://github.com/Nipuna-apps/Blue-Lione-Bot.git' //script link
+global.owner = ['775978468']
+global.ownertag = '775978468' //ur tag number
+global.botname = '🧸 𝚂𝙸𝙻𝙴𝙽 𝙱𝙾𝚃𝚉 🧸' //ur bot name
+global.linkz = "මම⍤⃝🤭─────💞🧸මලක්★᭄❥͜͡⃝ᶫᵒᵛᵉ⃝➤⃟̱̱̱̱̄̄̄̄😊" //your theme url which will be displayed on whatsapp
+global.websitex = "වදින්නෑ කාගෙවත් දෙපා➸⃟̀̀̀̀̀̀̀̀͡🌈️◈━━━━🤪▷ ●͙͙͙͙🌼යදින්නෑ කාගෙවත් සමා➸⃟̀̀̀̀̀̀̀🌈️◈━━━━🤬▷ ●͙͙͙͙͙͙͙🌼අහන්නෑ කාගෙවත් කතා➸🌈️⃟̀̀̀̀̀̀̀̀◈━━━━😬▷ ●͙͙͙͙͙͙͙🌼👍 " //ur website to be displayed
+global.botscript = '┃🎀⃝⃡ ʟ̶ᴏ̶ᵛ̶͠͠ᵉ̶ ᴍ̶ᵉ̶  ⃝⃡🎀 𝙾̶̷̈̇𝚁̶ 👀⃞⃖⃗ʜᴀ̶ᵗᵉ̶ ᴍ̶ᵉ̶⃞⃖⃗ 🍹  ᴅᴏ̶̷͢͢ⁿᵗ̶ོ ᴘ̷ʟ̶ᵃ͠ʸ̶ ᴡ̷̈͢͢͢ɪ̶ᵗʰ̶ ᴍ̶ᵉ̶͠   ⃤ོ' //script link
 global.reactmoji = "❤" //ur menu react emoji
-global.themeemoji = "🎸" //ur theme emoji
-global.packname = "⚜𝓑𝓵𝓾𝓮 𝓛𝓲𝓸𝓷𝓮⚜ Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "NIPUNA" //ur sticker watermark author
+global.themeemoji = "🧸" //ur theme emoji
+global.packname = "𝚂𝙸𝙻𝙴𝙽 𝙱𝙾𝚃 Stickers\n\n\n\n\n\n\n\n\n\n\n🤤🧸 " //ur sticker watermark packname
+global.author = "𝙼𝚁.𝚂𝙸𝙻𝙴𝙽" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -59,7 +59,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['714188730'] //ur premium numbers
+global.premium = ['775978468'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -83,7 +83,7 @@ global.ntilink = []
 global.sessionName = 'session'
 global.antitags = true
 global.prefa = ['','!','.','🐦','🐤','🗿']
-global.sp = '⚜'
+global.sp = '👻'
 global.mess = {
     success: 'Done ✓',
     admin: 'This Feature Is Only For Admin! 🇱🇰',
